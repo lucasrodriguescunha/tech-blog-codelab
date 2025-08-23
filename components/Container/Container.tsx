2453c0c7d061f74462fe60923c8d83481dda7204
lucasrodriguescunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2025-08-23 10:39:11 -0300

    feat: replace Header with HeroBanner and add ArticleList component to Container

--- components/Container/Container.tsx
+++ components/Container/Container.tsx
@@ -1,11 +1,13 @@
-import Header from '@/components/HeroBanner/HeroBanner';
+import HeroBanner from '@/components/HeroBanner/HeroBanner';
+import ArticleList from '@/components/ArticleList/ArticleList';
 
 import styles from './Container.module.css';
 
 export default function Container() {
     return (
         <div className={styles.container}>
-            <Header/>
+            <HeroBanner/>
+            <ArticleList/>
         </div>
     );
 }
